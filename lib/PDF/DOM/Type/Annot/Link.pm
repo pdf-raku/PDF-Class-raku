4e8caca8c947cff1fdb3b70c969d19a99c1255fa
David Warring <david.warring@gmail.com>
2015-07-26 04:19:31 +0000

    adding Annot doco

--- lib/PDF/DOM/Type/Annot/Link.pm
+++ lib/PDF/DOM/Type/Annot/Link.pm
@@ -7,10 +7,15 @@ class PDF::DOM::Type::Annot::Link
 
     use PDF::Object::Tie;
 
-    has Hash $!A is entry;
-    has $!Dest is entry(:required);
-    has Str $!H is entry;
-    has Hash $!PA is entry;
-    has Array $!QuadPoints is entry;
+    has Hash $!A is entry;           #| (Optional; PDF 1.1) An action to be performed when the link annotation is activated (see Section 8.5, “Actions”).
+    has $!Dest is entry(:required);  #| (Optional; not permitted if an A entry is present) A destination to be displayed when the annotation is activated
+    has Str $!H is entry;            #| (Optional; PDF 1.2) The annotation’s highlighting mode, the visual effect to be used when the mouse button is pressed or held down inside its active area:
+                                     #| N(None)    - No highlighting.
+                                     #| I(Invert)  - Invert the contents of the annotation rectangle.
+                                     #| O(Outline) - Invert the annotation’s border.
+                                     #| P(Push)    - Display the annotation as if it were being pushed below the surface of the page;
+                                     #| Default value: I.
+    has Hash $!PA is entry;          #| (Optional; PDF 1.3) A URI action
+    has Array $!QuadPoints is entry; #| (Optional; PDF 1.6) An array of 8 × n numbers specifying the coordinates of nquadrilaterals in default user space that comprise the region in which the link should be activated. The coordinates for each quadrilateral are given in the order: x1 y1 x2 y2 x3 y3 x4 y4, specifying the four vertices of the quadrilateral in counterclockwise order.
 
 }
--- lib/PDF/DOM/Type/Annot/Text.pm
+++ lib/PDF/DOM/Type/Annot/Text.pm
@@ -7,9 +7,13 @@ class PDF::DOM::Type::Annot::Text
 
     use PDF::Object::Tie;
 
-    has Bool $!Open is entry;
-    has Str $!Name is entry;
-    has Str $!State is entry;
-    has Str $!StateModel is entry;
+    # Seet [PDF 1.7 TABLE 8.23 Additional entries specific to a text annotation]
+    has Bool $!Open is entry;       #| (Optional) A flag specifying whether the annotation should initially be displayed open. Default value: false (closed).
+    has Str $!Name is entry;        #| (Optional) The name of an icon to be used in displaying the annotation. Viewer applications should provide predefined icon appearances for at least the following standard names:
+                                    #|  - Comment, Key, Note, Help, NewParagraph, Paragraph, Insert
+                                    #| Additional names may be supported as well. Default value: Note.
+    has Str $!State is entry;       #| (Optional; PDF 1.5) The state to which the original annotation should be set; see “Annotation States,” above.
+                                    #| Default: “Unmarked” if StateModel is “Marked”; “None” if StateModel is “Review
+    has Str $!StateModel is entry;  #| (Required if State is present, otherwise optional; PDF 1.5) The state model corresponding to State; see “Annotation States,” above
 
 }
