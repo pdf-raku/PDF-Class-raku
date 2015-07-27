1fe782a1a86e7581d2c00a1527bc5070501dc8b3
David Warring <david.warring@gmail.com>
2015-07-27 04:59:30 +0000

    adding function type doco

--- lib/PDF/DOM/Type/Function/PostScript.pm
+++ lib/PDF/DOM/Type/Function/PostScript.pm
@@ -3,7 +3,8 @@ use v6;
 use PDF::DOM::Type::Function;
 
 #| /FunctionType 4 - PostScript
-
+# see [PDF 1.7 Section 3.9.4 Type 4 (PostScript Calculator) Functions]
 class PDF::DOM::Type::Function::PostScript
     is PDF::DOM::Type::Function {
+
 }
