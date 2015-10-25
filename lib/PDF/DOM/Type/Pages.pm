3b8c2a48b293f4f5a07fda314e3da26055f60896
David Warring <david.warring@gmail.com>
2015-10-25 17:45:55 +0000

    dao import method no long takes an argument

--- lib/PDF/DOM/Type/Pages.pm
+++ lib/PDF/DOM/Type/Pages.pm
@@ -50,7 +50,7 @@ class PDF::DOM::Type::Pages
         }
         else {
             self.Kids.push: $page;
-	    $page<Parent> = self.import($page);
+	    $page<Parent> = self.import;
         }
 
         self<Count>++;
@@ -147,7 +147,7 @@ class PDF::DOM::Type::Pages
         my Array $kids = self.Kids;
         for $kids.keys {
             my $kid = $kids[$_];
-            $kid.<Parent> = self.import($kid);
+            $kid.<Parent> = self.import;
             $kid.cb-finish;
             $count += $kid.can('Count') ?? $kid.Count !! 1;
         }
