fe61f27942ed264cba1fb733cc4c534bbb433435
David Warring <david.warring@gmail.com>
2018-02-01 18:04:00 +1300

    Add PDF::Function::PostScript.calc

--- lib/PDF/Function/PostScript.pm
+++ lib/PDF/Function/PostScript.pm
@@ -14,4 +14,149 @@ class PDF::Function::PostScript
 	$/.ast
     }
 
+    method !clip(Numeric $v, Range $r) {
+        min($r.max, max($r.min, $v));
+    }
+
+    class Interpreter {
+        has @.stack;
+        method pop($type = Numeric) {
+            my $v = @!stack
+                ?? @!stack.pop
+                !! die "Postscript stack underflow";
+            die "typecheck error, expected {$type.perl}, got: {$v.perl}"
+                unless $v ~~ $type;
+            $v;
+        }
+        method push($v) {
+            @!stack < 100
+                ?? @!stack.push($v)
+                !! die "Postscript stack overflow"
+        }
+
+        multi method run(List :$expr!) {
+            self.run(|$_) for $expr.list;
+        }
+        multi method run(Int :$int! ) {
+            self.push($int);
+        }
+        multi method run(Numeric :$real! ) {
+            self.push($real);
+        }
+        multi method run(Bool :$bool! ) {
+            self.push($bool);
+        }
+
+        my Routine %Ops = BEGIN %(
+            # Arithmetic
+            add      => method { self.pop + self.pop },
+            sub      => method { my $n2 = self.pop; self.pop - $n2 },
+            mul      => method { self.pop * self.pop },
+            div      => method { my $n2 = self.pop; self.pop / $n2 },
+            idiv     => method { my $n2 = self.pop; self.pop div $n2 },
+            mod      => method { my $n2 = self.pop; self.pop % $n2 },
+            neg      => method { -self.pop },
+            abs      => method { self.pop.abs },
+            ceiling  => method { self.pop.ceiling },
+            floor    => method { self.pop.floor },
+            round    => method { self.pop.round },
+            truncate => method { self.pop.Int },
+            sqrt     => method { self.pop.sqrt },
+            sin      => method { sin(self.pop * pi / 180); },
+            cos      => method { cos(self.pop * pi / 180); },
+            atan     => method { my $den = self.pop;
+                                 my $num = self.pop;
+                                 $den = 0 if $den =~= 0;
+                                 my $angle = $den
+                                     ?? atan($num / $den) * 180 / pi
+                                     !! ($num =~= 0 ?? die "undefined result" !! 90);
+                                 $angle -= 180 if $den < 0;
+                                 $angle += 360 if $angle < 0;
+                                 $angle;
+                             },
+            exp      => method { my $n2 = self.pop; self.pop ** $n2 },
+            ln       => method { log self.pop; },
+            log      => method { log10 self.pop; },
+            cvr      => method { self.pop.Num },
+            cvi      => method { self.pop.Int },
+
+            # Relational, Boolean and Bitwise Operators
+            eq      => method { self.pop == self.pop },
+            ne      => method { self.pop != self.pop },
+            gt      => method { self.pop >  $_ with self.pop },
+            ge      => method { self.pop >= $_ with self.pop },
+            lt      => method { self.pop <  $_ with self.pop },
+            le      => method { self.pop <= $_ with self.pop },
+            and     => method { self.pop(Int) +& self.pop(Int) },
+            or      => method { self.pop(Int) +| self.pop(Int) },
+            xor     => method { self.pop(Int) +^ self.pop(Int) },
+            not     => method {
+                           given self.pop(Int) {
+                               $_ ~~ Bool ?? not $_  !! ($_ * -1) -1
+                           }
+                       },
+            bitshift => method { my $b = self.pop(Int); self.pop(Int) +< $b },
+            # Stack Operators
+            pop     => method {
+                self.pop(Any);
+                [];
+            },
+            exch     => method {
+                my $a = self.pop(Any);
+                my $b = self.pop(Any);
+                [$a, $b];
+            },
+            dup     => method {
+                my $a = self.pop(Any);
+                [$a, $a];
+            },
+            copy    => method {
+                my UInt $n = self.pop(Int);
+                die "stack underflow"
+                    unless @!stack >= $n;
+                @!stack.tail($n);
+            },
+            index    => method {
+                my UInt $i = self.pop(Int);
+                die "stack underflow"
+                    unless @!stack >= $i;
+                @!stack[* - ($i + 1)];
+            },
+            roll    => method {
+                my $j = self.pop(Int);
+                my UInt $n = self.pop(Int);
+                die "stack underflow"
+                    unless @!stack >= $n;
+                @!stack.splice(* - $n).rotate($j);
+            },
+            true    => method { True },
+            false   => method { False },
+           );
+        multi method run(Str :$op! ) {
+            with %Ops{$op} {
+                @!stack.append: .(self);
+            }
+            else {
+                die "unknown postscript operator: $op";
+            }
+        }
+        multi method run(Hash :$cond!) {
+            my $branch = $.pop(Bool)
+                ?? $cond<if>
+                !! $cond<else>;
+            $.run(|$_) with $branch;
+        }
+    }
+
+    #| run the calculator function
+    method calc(List $in, :$ast = self.parse) {
+        my Range @domain = $.Domain.map: -> $a, $b { Range.new($a, $b) };
+        my Range @range = $.Range.map: -> $a, $b { Range.new($a, $b) };
+        my Numeric @stack = ($in.list Z @domain).map: { self!clip(.[0], .[1]) };
+        my $interpreter = Interpreter.new: :@stack;
+        $interpreter.run: |$ast;
+        my $out = $interpreter.stack;
+        ($out.list Z @range).map: { self!clip(.[0], .[1]) };
+    }
+
 }
