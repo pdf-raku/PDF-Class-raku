fa806878bc7df7da30aef38b5275f04c9f91b844
David Warring <david.warring@gmail.com>
2018-04-08 07:49:45 +1200

    fix ToUnicode entry in type 0, 1 and 3 fonts

--- lib/PDF/Font/CIDFontType2.pm
+++ lib/PDF/Font/CIDFontType2.pm
@@ -1,7 +1,8 @@
 use v6;
-use PDF::Font::CIDFontType0;
-
+##use PDF::Font::CIDFontType0;
+use PDF::Font;
 class PDF::Font::CIDFontType2
-    is PDF::Font::CIDFontType0 {
+    is PDF::Font {
+##    is PDF::Font::CIDFontType0 {
     # see [PDF 1.7 TABLE 5.14 - has identical structure to CIDFontType0]
 }
