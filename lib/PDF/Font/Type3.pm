70c60d93b093022e6a285d3a28a6b31fd54db371
David Warring <david.warring@gmail.com>
2018-05-24 08:36:08 +1200

    rearrange use statements in type3 fonts
    it makes a difference in elimiinating 'P6M GLOBAL symbols failed'
    errors, as I've discovered, while chomping through sample PDF's in
    http://www.stillhq.com/pdfdb/db.html, using pdf-checker.p6

--- lib/PDF/Font/Type3.pm
+++ lib/PDF/Font/Type3.pm
@@ -10,6 +10,9 @@ class PDF::Font::Type3
     use PDF::COS::Dict;
     use PDF::COS::Name;
     use PDF::COS::Stream;
+    use PDF::FontDescriptor;
+    use PDF::Encoding;
+    use PDF::Resources;
 
     # see [PDF 1.7 TABLE 5.9 Entries in a Type 3 font dictionary]
     has PDF::COS::Name $.Name is entry;                         #| (Required in PDF 1.0; optional otherwise) See Table 5.8 on page 413
@@ -17,7 +20,6 @@ class PDF::Font::Type3
     has Numeric @.FontMatrix is entry(:required, :len(6));       #| (Required) An array of six numbers specifying the font matrix, mapping glyph space to text space
     has PDF::COS::Stream %.CharProcs is entry(:required);        #| (Required) A dictionary in which each key is a character name and the value associated with that key is a content stream that constructs and paints the glyph for that character.
 
-    use PDF::Encoding;
     my subset NameOrEncoding where PDF::COS::Name | PDF::Encoding;
     multi sub coerce(Hash $dict, NameOrEncoding) {
         PDF::COS.coerce($dict, PDF::Encoding);
@@ -30,10 +32,8 @@ class PDF::Font::Type3
 
     has Numeric @.Widths is entry(:required);          #| (Required) An array of (LastChar − FirstChar + 1) widths, each element being the glyph width for the character code that equals FirstChar plus the array index.
 
-    use PDF::FontDescriptor;
     has PDF::FontDescriptor $.FontDescriptor is entry(:indirect);     #| (Required in Tagged PDF documents; must be an indirect reference) A font descriptor describing the font’s default metrics other than its glyph widths
 
-    use PDF::Resources;
     has PDF::Resources $.Resources is entry;   #| (Optional but strongly recommended; PDF 1.2) A list of the named resources, such as fonts and images, required by the glyph descriptions in this font
 
     has PDF::COS::Stream $.ToUnicode is entry;        #| (Optional; PDF 1.2) A stream containing a CMap file that maps character codes to Unicode values
