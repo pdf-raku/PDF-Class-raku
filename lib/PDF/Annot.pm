43e25d576a4ceb72d2291f3723de76873819267f
David Warring <david.warring@gmail.com>
2018-08-11 19:50:49 +1200

    Annot.P is of type PDF::Page

--- lib/PDF/Annot.pm
+++ lib/PDF/Annot.pm
@@ -2,6 +2,7 @@ use v6;
 
 use PDF::COS::Tie::Hash;
 use PDF::Class::Type;
+use PDF::Page;
 
 #| /Type /Annot Annotations
 #| See [PDF 1.7 Section 8.4.1 - Annotation Dictionaries ]
@@ -23,9 +24,9 @@ class PDF::Annot
         'Stamp'|'Caret'|'Ink'|'Popup'|'FileAttachment'|'Sound'|'Movie'|'Widget'|'Screen'|
         'PrinterMark'|'TrapNet'|'Watermark'|'Redact';
     has AnnotName $.Subtype is entry(:required);
-    has Numeric @.Rect is entry(:required);                     #| (Required) The annotation rectangle, defining the location of the annotation on the page in default user space units.
+    has Numeric @.Rect is entry(:required, :len(4));                     #| (Required) The annotation rectangle, defining the location of the annotation on the page in default user space units.
     has PDF::COS::TextString $.Contents is entry;               #| (Optional) Text to be displayed for the annotation or, if this type of annotation does not display text, an alternate description of the annotation’s contents in human-readable form
-    has Hash $.P is entry(:alias<page>);                        #| (Optional; PDF 1.3; not used in FDF files) An indirect reference to the page object with which this annotation is associated.
+    has PDF::Page $.P is entry(:alias<page>);                        #| (Optional; PDF 1.3; not used in FDF files) An indirect reference to the page object with which this annotation is associated.
     has PDF::COS::TextString $.NM is entry(:alias<annotation-name>);       #| (Optional; PDF 1.4) The annotation name, a text string uniquely identifying it among all the annotations on its page.
     my subset DateOrTextString where PDF::COS::DateString | PDF::COS::TextString;
     multi sub coerce(Str $s is rw, DateOrTextString) {
